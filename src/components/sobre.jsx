efde01f5ef45349fdce8008d7fc4289ab81c0fab
Rafael Rios <108680246+RafaelRaios@users.noreply.github.com>
2023-05-30 19:06:01 -0300

    fonte

--- src/components/sobre.jsx
+++ src/components/sobre.jsx
@@ -12,7 +12,7 @@ const Sobre = () => {
                 </div>
                 
             
-                <p className="mt-20">
+                <p className="mt-20 font-base">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
@@ -23,7 +23,7 @@ const Sobre = () => {
 
                 <br />
 
-                <p className="py-10 mb-10 md:mb-0">
+                <p className="py-10 mb-10 md:mb-0 font-base">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
                   dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
