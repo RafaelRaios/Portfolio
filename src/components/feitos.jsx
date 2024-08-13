df0f343de1921a5d6afe61076966c0fb496053ec
Rafael Rios <108680246+RafaelRaios@users.noreply.github.com>
2024-08-13 20:17:54 -0300

    Update feitos.jsx

--- src/components/feitos.jsx
+++ src/components/feitos.jsx
@@ -5,7 +5,8 @@ import site from "../assets/images/site.png"
 import splotify from "../assets/images/splotify.png"
 import paine_cards from "../assets/images/painel-cards.png"
 import filter_project from "../assets/images/filter-project.png"
-import { unmount } from "react-scroll/modules/mixins/scroller";
+import bd_cinema from "../assets/images/BDCinema.jpg"
+import alagoano2 from "../assets/images/alagoano2.png"
 
 const Feitos = () => {
     const [index, setIndex] = useState(0)
@@ -92,7 +93,7 @@ const Feitos = () => {
                         <img src={paine_cards} alt="" link/>
                     </a>
                     <a href="https://painel-cards.vercel.app/" target="blank">
-                        <p className="text-white my-4 font-base justify-center items-center cursor-pointer" >
+                        <p className="text-white my-4 font-base justify-center items-center cursor-pointer hover:text-cyan-200" >
                         Painel Cards é um mini site feito por mim para treinar a componentização no Angular. Clicando na imagem, você será direcionado para o link do repositório no GitHub, e clicando neste texto, você irá para o site diretamente.
                         </p>
                     </a>
@@ -126,14 +127,50 @@ const Feitos = () => {
                         <img src={filter_project} alt="" link/>
                     </a>
                     <a href="https://filter-project-ashy.vercel.app/" target="blank">
-                        <p className="text-white my-4 font-base justify-center items-center cursor-pointer" >
+                        <p className="text-white my-4 font-base justify-center items-center cursor-pointer hover:text-cyan-200" >
                         Filter Project é um projeto em Angular de um site que filtra as pessoas cadastradas no banco de dados local e disponibiliza as informações da pessoa selecionada. Clicando na imagem, você será direcionado para o link do repositório no GitHub, e clicando neste texto, você irá para o site diretamente.
                         </p>
                     </a>
                 </div>
             )
         },
+        {
+            img: bd_cinema,
+            child: (
+                <div className="scale-25 md:scale-75 bg-gradient-to-t from-gray-900/90 from-45% to-neutral-800/90 p-4 rounded-md items-center
+                max-w-screen-lg mx-auto">
+                    <h2 className="text-white text-2xl md:text-4xl font-signature py-3 mx-4 md:mx-10 md:my-4">Banco de Dados Cinema</h2>
+                    <a href="https://github.com/RafaelRaios/BD-Cinema" target="blank"
+                    className="flex scale-95 items-center">
+                        <img src={bd_cinema} alt="" link/>
+                    </a>
+                    <p className="text-white my-4 font-base justify-center items-center " >
+                    Como o nome já indica, este projeto refere-se ao banco de dados de um cinema, desenvolvido para a disciplina de Gerenciamento de Dados e Informações. O projeto é abrangente e inclui tabelas bem estruturadas, uma série de comandos de inserção e algumas funções e triggers que atendem de forma verossímil às necessidades de um cinema real.
+                    </p>
+                    
+                </div>
+            )
+        },
+        {
+            img: alagoano2,
+            child: (
+                <div className="scale-25 md:scale-75 bg-gradient-to-t from-gray-900/90 from-45% to-neutral-800/90 p-4 rounded-md items-center
+                max-w-screen-lg mx-auto">
+                    <h2 className="text-white text-2xl md:text-4xl font-signature py-3 mx-4 md:mx-10 md:my-4 ">Alagoaninho Adventures 2</h2>
+                    <a href="https://editor.p5js.org/RafaelRaios/sketches/b5i9I5gXR" target="blank"
+                    className="flex scale-95 ml-20">
+                        <img src={alagoano2} alt="" link/>
+                    </a>
+                    <a href="https://editor.p5js.org/RafaelRaios/full/b5i9I5gXR" target="blank">
+                        <p className="text-white my-4 font-base justify-center items-center cursor-pointer hover:text-cyan-200" >
+                        Alagoaninho Adventures 2 é uma pseudocontinuação do primeiro jogo, desenvolvido para a disciplina de Introdução à Multimídia. Desta vez, o projeto utiliza o editor p5.js. O jogo mantém o mesmo estilo do anterior, sendo um jogo travesso que interage de maneira criativa com o cenário.
 
+                        Observação: Clicando na imagem abaixo, você será redirecionado para o link do código no p5.js. Se clicar no texto, poderá jogar o jogo em tela cheia no seu navegador.
+                        </p>
+                    </a>
+                </div>
+            )
+        },
     ]
 
     return (
